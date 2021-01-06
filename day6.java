e3f371d8771f89b57d57b7c656f29dfe0e97248c
Your Name <you@example.com>
2021-01-06 19:21:33 +0530

    Day 6 problem added

---
+++ day6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findKthPositive(int[] a, int k) {
+        int n=a.length;
+        boolean[] freq=new boolean[2002];
+        Arrays.fill(freq,false);
+        for(int i=0;i<n;++i)
+        {
+            int val=a[i];
+            freq[val]=true;
+        }
+        int i=0;
+        while(k>0)
+        {
+            ++i;
+            if(!freq[i])
+            {
+                k--;
+            }
+        }
+        return i; 
+    }
+}
